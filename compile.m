fdc98daea8bd73f7be4cc47c881c4af85427a324
Kyle Kloster <kyle.kloster@gmail.com>
2016-02-19 14:46:05 -0500

    Adding rest of mex and M scripts for computing ppr paths and grid

--- compile.m
+++ compile.m
@@ -5,7 +5,6 @@ try
     cd(mypath);
     if ismac
 %         mex -O CXXFLAGS="\$CXXFLAGS -Wall -I. -std=c++11" -largeArrayDims ppr_fast_grid_mex.cpp
-        %mex -O CXXFLAGS="\$CXXFLAGS -Wall -I. -std=c++11" -largeArrayDims ppr_paths_mex.cpp
         %mex -O CXXFLAGS="\$CXXFLAGS -Wall -I. -std=c++11" -largeArrayDims ppr_grid_mex.cpp
 %         mex -O CXXFLAGS="\$CXXFLAGS -Wall -I. -std=c++11" -largeArrayDims ppr_grid2_mex.cpp
 %         mex -O CXXFLAGS="\$CXXFLAGS -Wall -I. -std=c++11" -largeArrayDims pprgrow_mex.cc
@@ -13,11 +12,11 @@ try
         mex -O CXXFLAGS="\$CXXFLAGS -Wall -I. -std=c++11" -largeArrayDims ./util/graphcutsweep_val.c
     else
         mex -O CXXFLAGS="\$CXXFLAGS -Wall -std=c++0x" -I. -largeArrayDims ppr_fast_grid_mex.cpp
-        %mex -O CXXFLAGS="\$CXXFLAGS -Wall -std=c++0x" -I. -largeArrayDims ppr_paths_mex.cpp
         %mex -O CXXFLAGS="\$CXXFLAGS -Wall -std=c++0x" -I. -largeArrayDims ppr_grid_mex.cpp
         mex -O CXXFLAGS="\$CXXFLAGS -Wall -std=c++0x" -I. -largeArrayDims ppr_grid2_mex.cpp
         mex -O CXXFLAGS="\$CXXFLAGS -Wall -std=c++0x" -I. -largeArrayDims pprgrow_mex.cc
         mex -O CXXFLAGS="\$CXXFLAGS -Wall -std=c++0x" -I. -largeArrayDims ppr_paths_rho_mex.cpp
+        mex -O CXXFLAGS="\$CXXFLAGS -Wall -I. -std=c++11" -largeArrayDims ./util/graphcutsweep_val.c
     end
     cd(curdir);
 catch me
